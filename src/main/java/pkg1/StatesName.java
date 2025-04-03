62185011530604529231642b52b2e69abe0664d7
Sarita Jha <jhasarita016@gmail.com>
2025-04-03 11:37:29 -0400

    add output.txt

--- src/main/java/pkg1/StatesName.java
+++ src/main/java/pkg1/StatesName.java
@@ -1,7 +1,8 @@
 package pkg1;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,10 +12,14 @@ import java.util.Scanner;
 
 public class StatesName {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		File f1 = new File("USAStates.txt");
-		
+	public static void main(String[] args) throws IOException {
+		jumbleFile("USAStates.txt");
+	}
 
+	public static String jumbleFile(String fName) throws IOException {
+		File f1 = new File(fName);
+		FileWriter fw1 = new FileWriter("output.txt");
+		String info1 = "";
 		List<String> states = new ArrayList<>();
 
 		Scanner sc1 = new Scanner(f1);
@@ -22,9 +27,30 @@ public class StatesName {
 			String inputString = sc1.nextLine();
 
 			System.out.print(inputString + " - ");
-			System.out.println(jumbleWord(inputString).toLowerCase());
+			info1 = jumbleMultiWords(inputString).toLowerCase();
+			System.out.println(info1);
+			fw1.write(info1 + "\n");
 		}
+
 		System.out.println("inter state");
+		fw1.close();
+		return "success";
+
+	}
+
+	public static String jumbleMultiWords(String input) {
+		String output = "";
+
+		if (input == null || input.isEmpty()) {
+			return input;
+		}
+
+		List<String> list1 = Arrays.asList(input.split(" "));
+		int size = list1.size();
+		for (int i = 0; i < size; i++) {
+			output = output + " " + jumbleWord(list1.get(i));
+		}
+		return output;
 
 	}
 
@@ -32,9 +58,10 @@ public class StatesName {
 		if (input == null || input.isEmpty()) {
 			return input;
 		}
-		List<String> list1 = Arrays.asList(input.split(""));
-		Collections.shuffle(list1, new Random());
-		return String.join("", list1);
+
+		List<String> list2 = Arrays.asList(input.split(""));
+		Collections.shuffle(list2, new Random());
+		return String.join("", list2);
 
 	}
 
